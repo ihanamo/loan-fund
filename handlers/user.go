e048c96c52d916575309be2e4a933b59c7ac976b
ihanamo <hnnemntzr200@gmail.com>
2024-08-21 13:23:30 +0330

    GENERATE jwt added

--- handlers/user.go
+++ handlers/user.go
@@ -1,1 +1,27 @@
 package handlers
+
+import (
+	"loan-fund/models"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var JWTKey = []byte("secret_key")
+
+func GenerateJWT(customer models.User) (string, error) {
+	claims := &jwt.MapClaims{
+		"customer-id": customer.ID,
+		"username":    customer.Username,
+		"exp":         time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenStr, err := token.SignedString(JWTKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
+}
