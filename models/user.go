593ff07b2da3f89fea62d89ce3f0e8f80978c70e
ihanamo <hnnemntzr200@gmail.com>
2024-08-21 11:33:54 +0330

    user struct created

--- models/user.go
+++ models/user.go
@@ -1,2 +1,19 @@
 package models
 
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	ID         uint   `json:"user-id" gorm:"primaryKey;autoIncrement"`
+	FirstName  string `json:"firstname" gorm:"type:varchar(100);not null"`
+	LastName   string `json:"lastname" gorm:"type:varchar(100);not null"`
+	Username   string `json:"username" gorm:"type:varchar(100);unique;not null"`
+	NationalID string `json:"nationalid" gorm:"type:varchar(100);unique;not null"`
+	Phone      string `json:"phone" gorm:"type:varchar(100);not null"`
+	Password   string `json:"password" gorm:"type:varchar(255);not null"`
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
