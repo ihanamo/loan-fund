9e003877e8d181ffc5d144212e03d2a04342a261
ihanamo <hnnemntzr200@gmail.com>
2024-09-28 18:05:48 +0330

    log model added

--- models/log.go
+++ models/log.go
@@ -1,1 +1,15 @@
 package models
+
+import "time"
+
+// Log model to track user actions such as loan issuance, repayments, deposits
+type Log struct {
+	ID            uint        `json:"log_id" gorm:"primaryKey;autoIncrement"`
+	UserID        uint        `json:"user_id" gorm:"not null"`
+	User          User        `gorm:"foreignKey:UserID"`
+	TransactionID uint        `json:"transaction_id" gorm:"not null"`
+	Transaction   Transaction `gorm:"foreignKey:TransactionID"`
+	Type          string      `json:"type" gorm:"not null"` // "loan", "repayment", "deposit"
+	CreatedAt     time.Time   `json:"created_at"`
+}
+
